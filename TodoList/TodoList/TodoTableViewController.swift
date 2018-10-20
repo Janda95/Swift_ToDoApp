0a035b442e76328c9c29ad42f40330a7d5e8fe39
Aiyana Rettinghouse <aiyana@pegasus.local>
2018-10-20 15:47:28 -0700

    todo tableview implementation began

--- TodoList/TodoList/TodoTableViewController.swift
+++ TodoList/TodoList/TodoTableViewController.swift
@@ -1,2 +1,24 @@
+import UIKit
 
-import Foundation
+//import class for table view
+
+class TodoTableViewController: UITableViewController {
+    
+    //var todoArray: [Activities/Items] =
+    var tempPlaceholder: [String] = ["Item 1", "Item 2"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return todoArray.count
+        return tempPlaceholder.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let
+        let placeholder = tempPlaceholder[indexPath.row]
+        return placeholder
+    }
+    
+    
+    
+}
+
