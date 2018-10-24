a012e1e769e75bb4161f6bd74c7e01b0c6d2f424
Aiyana Rettinghouse <aiyana@pegasus.local>
2018-10-24 15:17:09 -0700

    list elements are showing

--- TodoList/TodoList/TodoTableViewController.swift
+++ TodoList/TodoList/TodoTableViewController.swift
@@ -8,14 +8,19 @@ class TodoTableViewController: UITableViewController {
     var tempPlaceholder: [String] = ["Item 1", "Item 2"]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         //return todoArray.count
         return tempPlaceholder.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let
-        let placeholder = tempPlaceholder[indexPath.row]
-        return placeholder
+        let indexRow = tempPlaceholder[indexPath.row]
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell")!
+        cell.textLabel?.text = indexRow
+        
+        return cell
     }
     
     
