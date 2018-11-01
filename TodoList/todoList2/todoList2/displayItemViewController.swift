ae6bceb5bfd62aead21cd688041ef625738743f2
Aiyana Rettinghouse <aiyana@pegasus.local>
2018-11-01 13:35:23 -0700

    Text Field Replacing Label

--- TodoList/todoList2/todoList2/displayItemViewController.swift
+++ TodoList/todoList2/todoList2/displayItemViewController.swift
@@ -2,12 +2,14 @@ import UIKit
 
 class DisplayItemViewController: UIViewController {
     
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var ItemDescription: UILabel!
     
     var textDescription: String = "Error: Seque Didn't Work"
     
     override func viewWillAppear(_ animated: Bool) {
         ItemDescription.text = textDescription
+        textField.text = textDescription
     }
     
 }
